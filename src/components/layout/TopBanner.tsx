de39f81fdc20a0c2e3013ab9d8f4b689261ad177
councicroff-commits <councicroff@gmail.com>
2026-09-19 22:17:15 +0800

    Update TopBanner.tsx

--- src/components/layout/TopBanner.tsx
+++ src/components/layout/TopBanner.tsx
@@ -33,7 +33,7 @@ const TopBanner: React.FC = () => {
     const fetchBannerSettings = async () => {
       try {
         const res = await fetch(
-          'https://cc-backend-yc-team.onrender.com/api/v1/parts/'
+          'https://cc-backend-yc-team.onrender.com/api/v1/parts'
         );
 
         if (!res.ok) {
